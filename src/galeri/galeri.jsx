e5e6ace0147977e9cf1a01638fc47c7cc1f9f1e0
deniPamungkas <pamungkasdeni18@gmail.com>
2023-12-20 09:20:54 +0700

    adf

--- src/galeri/galeri.jsx
+++ src/galeri/galeri.jsx
@@ -1,15 +1,17 @@
-
 const Galeri = () => {
-    const data = [1,2,3,4,5,6,7,8,9]
+  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   return (
-    <section className="galeri flex gap-x-4 overflow-scroll absolute w-full left-0">
-        {
-            data.map((item,index)=>{
-                return <div key={index} className="w-52 h-52 rounded-lg shrink-0 bg-red-400"></div>
-            })
-        }
+    <section className="galeri flex gap-x-4 overflow-scroll absolute w-full left-0 px-5">
+      {data.map((item, index) => {
+        return (
+          <div
+            key={index}
+            className="w-52 h-52 rounded-lg shrink-0 bg-red-400"
+          ></div>
+        );
+      })}
     </section>
-  )
-}
+  );
+};
 
-export default Galeri
+export default Galeri;
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,11 +1,13 @@
 const About = () => {
   return (
     <section className="h-full px-5 md:flex md:flex-row-reverse md:justify-end m-auto md:gap-x-10 pb-52 pt-20 xl:w-3/4">
-      <img
-        src="fotoku.jpg"
-        alt=""
-        className="w-40 h-40 rounded-full object-cover object-bottom mb-7"
-      />
+      <div className=" w-52 h-52 md:w-64 md:h-64 flex justify-center items-start overflow-hidden rounded-full mb-5 md:mb-0 shrink-0">
+        <img
+          src="fotoku.jpg"
+          alt=""
+          className="w-64 h-64 object-cover object-bottom mb-7 -translate-y-10 md:-translate-y-0"
+        />
+      </div>
       <div className="md:w-2/3 dark:text-white">
         <h1 className="text-3xl font-bold md:text-5xl mb-7 md:mb-10">
           I’m Deni Pamungkas, an Indonesian 🇮🇩 Developer.
